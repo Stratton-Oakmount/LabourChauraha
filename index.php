706883f194545e0e87fb60fea48ff640c359abfe
Aryan Parekh <42067956+Aryan-S-Parekh@users.noreply.github.com>
2020-09-19 03:50:50 +0530

    Add files via upload
    first 3:50

---
+++ index.php
@@ -0,0 +1,71 @@
+<?xml version="1.0"?>
+<html >
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+
+    <title>LabourChauraha</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+
+    </style>
+</head>
+
+<body >
+<!-- Main -->
+<div id="main" class="box">
+<img src="design/imgh.jpeg" alt="" width="770" height="150" />
+<?php 
+include "menu.php"
+?>   
+<!-- Page (2 columns) -->
+
+        <div id="strip" class="box noprint">
+
+            <!-- RSS feeds -->
+            <hr class="noscreen" />
+
+            <!-- Breadcrumbs -->
+            <p id="breadcrumbs">&nbsp;</p>
+          <hr class="noscreen" />
+            
+        </div> <!-- /strip -->
+
+      
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+            
+            <!-- Article -->
+           
+            <!-- /article -->
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="article">
+                <h2><span><a href="#">Welcome To LabourChauraha</a></span></h2>
+               
+
+                <p align="justify"><font face="serif" size="3">There are an estimated 139 million migrants in the country according to the World Economic Forum. The International Labour Organization (ILO) predicted that due to the pandemic and the lockdown most of these workers would be poverty-stricken. Due to the pandemic people need to follow social distancing norms and in lieu of this it is imperetive that certain things need to be improved.</p><p> The labourers who had to meet at certain chowks for their employement are at a great risk. to protect these daily wagers what solution woud be more apt than an electronic system between the workers and the employers. To address this gaping need we have tried to develop this website which tries to incorporate aadhar verification, simple registration and seletion of workers. Here workers can register themselves with a simple OTP service and then the employers can eventually select them based on their pay and skill for a specific location.</font></p>
+                      <p align="right"> <img src="design/img1.jpg" alt="" width="431" height="200" /></p>
+              <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+            <hr class="noscreen" />
+            
+       </body>
+</html>
