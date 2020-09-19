522dd54c21c9b57c387c8a7949c92907ae384195
Aryan Parekh <42067956+Aryan-S-Parekh@users.noreply.github.com>
2020-09-19 06:15:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -27,37 +27,12 @@ include "menu.php"
 ?>   
 <!-- Page (2 columns) -->
 
-        <div id="strip" class="box noprint">
-
-            <!-- RSS feeds -->
-            <hr class="noscreen" />
-
-            <!-- Breadcrumbs -->
-            <p id="breadcrumbs">&nbsp;</p>
-          <hr class="noscreen" />
+        
             
-        </div> <!-- /strip -->
-
-      
-           
-            <!-- /article -->
-
-            <hr class="noscreen" />
-
-           
-            <!-- /article -->
-
-            <hr class="noscreen" />
-            
-            <!-- Article -->
-           
-            <!-- /article -->
-
-            <hr class="noscreen" />
-
+          
             <!-- Article -->
             <div class="article">
-                <h2><span><a href="#">Welcome To LabourChauraha</a></span></h2>
+                <h1><span><a href="#">Welcome To LabourChauraha</a></span></h1>
                
 
                 <p align="justify"><font face="serif" size="3">There are an estimated 139 million migrants in the country according to the World Economic Forum. The International Labour Organization (ILO) predicted that due to the pandemic and the lockdown most of these workers would be poverty-stricken. Due to the pandemic people need to follow social distancing norms and in lieu of this it is imperetive that certain things need to be improved.</p><p> The labourers who had to meet at certain chowks for their employement are at a great risk. to protect these daily wagers what solution woud be more apt than an electronic system between the workers and the employers. To address this gaping need we have tried to develop this website which tries to incorporate aadhar verification, simple registration and seletion of workers. Here workers can register themselves with a simple OTP service and then the employers can eventually select them based on their pay and skill for a specific location.</font></p>
