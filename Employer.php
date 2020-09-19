60430111418dd5f5e9c864750dcb04eb717d27a0
NeelBanga <70590905+NeelBanga@users.noreply.github.com>
2020-09-19 12:08:56 +0530

    Add files via upload

---
+++ Employer.php
@@ -0,0 +1,103 @@
+<?xml version="1.0"?>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+
+   
+    <title>LabourChauraha</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+.style2 {font-weight: bold}
+-->
+    </style>
+</head>
+
+<body >
+<!-- Main -->
+<div id="main" class="box">
+<img src="design/imgh.jpeg" alt="" width="770" height="150" />
+<?php 
+include "menu.php"
+?>   
+
+
+<div class="article">
+                <h2><span><a href="#">Employer</a></span></h2>
+<div class="login">
+                <form name="form1" method="post" action="sender2.php">
+                  <table width="100%" border="0">
+                    <tr>
+                      <td><strong>Name</strong></td>
+                    </tr>
+                    <tr>
+        <td><span id="sprytextfield1">
+                        <label>
+                        <input type="varchar" name="varname" id="varname">
+                        </label>
+                      <span class="textfieldRequiredMsg">*</span></span>
+          <label></label></td>
+                    </tr>
+                    <tr>
+                      <td><strong>Aadhar</strong></td>
+                    </tr>
+                    <tr>
+                <td><span id="sprytextfield2">
+                        <label>
+                        <input type="varchar" name="varaadhar" id="varaadhar">
+                        </label>
+                      <span class="textfieldRequiredMsg">*</span></span></td>
+                    </tr>
+                    
+                    
+                    
+					<tr>
+                      <td><strong>Location</strong></td>
+                    </tr>
+                    <tr>
+                     <td><span id="sprytextfield2">
+                        <label>
+                        <input type="varchar" name="varlocation" id="varlocation">
+                        </label>
+                      <span class="textfieldRequiredMsg">*</span></span></td>
+                    </tr>
+<tr>
+                      <td><strong>Phone Number</strong></td>
+                    </tr>
+                    <tr>
+                     <td><span id="sprytextfield3">
+                        <label>
+                        <input type="varchar" name="varphone" id="varlphone">
+                        </label>
+                      <span class="textfieldRequiredMsg">*</span></span></td>
+                    </tr>
+                    
+                    <tr>
+                      <td><label>
+                        <div align="center">
+                          <input type="submit" name="button" id="button" value="Store">
+                          </div>
+                      </label></td>
+                    </tr>
+                  </table>
+      </form>
+              </div>
+                <br/>
+     
+          </div> <!-- /col-in -->
+</div> <!-- /col -->
+
+</div> <!-- /main -->
+
