9ed635778290c639559b5768c161fc0be7519382
NeelBanga <70590905+NeelBanga@users.noreply.github.com>
2020-09-19 07:45:14 +0530

    Create AboutUs.php

---
+++ AboutUs.php
@@ -0,0 +1,69 @@
+<?xml version="1.0"?>
+<html>
+<head>
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+    <meta http-equiv="content-language" content="cs" />
+    <meta name="robots" content="all,follow" />
+
+    <title>LauborChauraha</title>
+    <meta name="description" content="..." />
+    <meta name="keywords" content="..." />
+    
+    <link rel="index" href="./" title="Home" />
+    <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
+    <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+    <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+    <style type="text/css">
+<!--
+.style1 {
+	color: #000066;
+	font-weight: bold;
+}
+-->
+    </style>
+</head>
+
+<body >
+<!-- Main -->
+<div id="main" class="box">
+<img src="design/imgh.jpeg" alt="" width="770" height="150" />
+<?php 
+include "menu.php"
+?>   
+
+    
+
+
+            <hr class="noscreen" />
+
+            <!-- Article -->
+            <div class="article">
+               <h1><span><a href="#">About Us</a></span></h1>
+               
+
+                <p align="justify"><font face="serif" size="3">Indian migrant workers during the COVID-19 pandemic have faced multiple hardships. With factories and workplaces shut down due to the lockdown imposed in the country, millions of migrant workers had to deal with the loss of income, food shortages and uncertainty about their future. Following this, many of them and their families went hungry. Thousands of them then began walking back home, with no means of transport due to the lockdown.</p>
+
+                    <p align="justify"><font face="serif" size="3">Employment rates has been very low leading many people to be unemployed and been pushed towards harsh labour. We all know how hard it is for them in harsh conditions and on top of that they are haunted by job insecurity. We are creating a platform where labours and employers can come in contact and make their lives a little easier.
+Labour market discrimination is defined as a situation where workers or groups of workers are treated differently in terms of recruitment, pay, benefits and promotion from other workers or groups due to their non-economic characteristics, including gender, race, religion and age. This means that while workers may be equally productive, they are not treated the same.
+Also, some workers who fear they may be discriminated against may not seek employment in those firms that they perceive practice discrimination. Hence they do not supply their labour to those types of firms. This shifts the potential supply curve to the left, and raises the relative wage rate of the favoured group.
+So, by providing more employment oppurtunities to the labours we can potentially increase the overall wage of the labours.
+Since the boom of internet we need to build a network or a community of this kind in internet and our website allows us to do just that.</p>
+              </p>
+              <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+            <hr class="noscreen" />
+            
+        </div> <!-- /content -->
+
+
+
+
+    </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+
+</div> <!-- /main -->
+
+</body>
+</html>
